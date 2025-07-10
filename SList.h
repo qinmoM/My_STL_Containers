1f22a61baeba190368554d138746faa47a5acfa0
qinmoM <ma200511@qq.com>
2025-07-10 16:50:27 +0800

    List Container

---
+++ SList.h
@@ -0,0 +1,260 @@
+#pragma once
+
+template<class T>
+struct SNode
+{
+    T data;
+    SNode<T>* next;
+
+    SNode() : data(T()), next(nullptr) { }
+
+    SNode(T data) : data(data), next(nullptr) { }
+};
+
+template<class T>
+class SList
+{
+protected:
+    SNode<T>* head;
+    int Size; 
+public:
+    SList()
+    {
+        head = new SNode<T>();
+        Size = 0;
+    }
+
+    ~SList()
+    {
+        SNode<T>* curr = head;
+        while (nullptr != head)
+        {
+            curr = head->next;
+            delete head;
+            head = curr;
+        }
+    }
+
+    inline bool empty() const
+    {
+        return !Size;
+    }
+
+    inline int size() const
+    {
+        return Size;
+    }
+
+    inline SNode<T>* begin() const
+    {
+        return head->next;
+    }
+
+    inline void push_front(T data) // O(1)
+    {
+        SNode<T>* newNode = new SNode<T>(data);
+        newNode->next = head->next;
+        head->next = newNode;
+        Size++;
+    }
+
+    inline void pop_front() // O(1)
+    {
+        if (0 == Size)
+        {
+            return;
+        }
+        SNode<T>* temp = head->next;
+        head->next = temp->next;
+        delete temp;
+        Size--;
+    }
+
+    void push_back(T data) // O(n)
+    {
+        SNode<T>* newNode = new SNode<T>(data);
+        SNode<T>* curr = head;
+        while (nullptr != curr->next)
+        {
+            curr = curr->next;
+        }
+        curr->next = newNode;
+        Size++;
+    }
+
+    void pop_back() // O(n)
+    {
+        if (0 == Size)
+        {
+            return;
+        }
+        SNode<T>* curr = head;
+        while (nullptr != curr->next->next)
+        {
+            curr = curr->next;
+        }
+        delete curr->next;
+        curr->next = nullptr;
+        Size--;
+    }
+
+    void insert(int index, T data) // O(n)
+    {
+        if (index < 0 || index > Size)
+        {
+            return;
+        }
+        SNode<T>* newNode = new SNode<T>(data);
+        SNode<T>* curr = head;
+        while (index) // if index is not zero
+        {
+            curr = curr->next;
+            index--;
+        }
+        newNode->next = curr->next;
+        curr->next = newNode;
+        Size++;
+    }
+
+    void remove(int index) // O(n)
+    {
+        if (index < 0 || index >= Size)
+        {
+            return;
+        }
+        SNode<T>* curr = head;
+        while (index) // if index is not zero
+        {
+            curr = curr->next;
+            index--;
+        }
+        SNode<T>* temp = curr->next;
+        curr->next = temp->next;
+        delete temp;
+        Size--;
+    }
+
+    inline void clear()
+    {
+        while (nullptr != head->next)
+        {
+            pop_front();
+        }
+    }
+
+    void reverse() // O(n)
+    {
+        if (2 > Size)
+        {
+            return;
+        }
+        SNode<T>* curr = head->next->next;
+        SNode<T>* last = head->next;
+        while (nullptr != curr)
+        {
+            last->next = curr->next;
+            curr->next = head->next;
+            head->next = curr;
+            curr = last->next;
+        }
+    }
+
+    // Find the index from the end of the list // Here is a demonstration of the situation without size:
+    SNode<T>* endAt(int index) // O(n)
+    {
+        if (0 >= index)
+        {
+            return nullptr;
+        }
+        SNode<T>* curr = head;
+        SNode<T>* result = head;
+        while (nullptr != curr)
+        {
+            if (nullptr != curr)
+            {
+                curr = curr->next;
+            }
+            else
+            {
+                return nullptr;
+            }
+
+            if (0 == index)
+            {
+                result = result->next;
+            }
+            else
+            {
+                index--;
+            }
+        }
+        if (index)
+        {
+            return nullptr;
+        }
+        else
+        {
+            return result;
+        }
+    }
+
+    // merge two sorted lists // O(n)
+    void merge(SList<T>& other, bool ascending = true)
+    {
+        SNode<T>* curr = head;
+        SNode<T>* temp = other.head->next;
+        while (nullptr != curr)
+        {
+            if (nullptr == curr->next)
+            {
+                curr->next = temp;
+                other.head->next = nullptr;
+                Size += other.Size;
+                other.Size = 0;
+                break;
+            }
+            if (nullptr == temp)
+            {
+                break;
+            }
+            if (ascending ? (curr->next->data >= temp->data) : (curr->next->data <= temp->data))
+            {
+                other.head->next = temp->next;
+                temp->next = curr->next;
+                curr->next = temp;
+                curr = curr->next;
+                temp = other.head->next;
+                Size++;
+                other.Size--;
+            }
+            else
+            {
+                curr = curr->next;
+            }
+        }
+        other.~SList();
+    }
+
+    // Judge whether these is a loop in the list.If not,return nullptr.Conversely,return the beginning of the loop. // O(n)
+    SNode<T>* hasLoop()
+    {
+        SNode<T>* slow = head;
+        SNode<T>* fast = head;
+        while (nullptr != fast && nullptr != fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                slow = head;
+                while (slow != fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Array.h"
+#include "SList.h"
 
 // Array
 void print(const Array<int>& arr)
@@ -17,24 +18,79 @@ void print(const Array<int>& arr)
     std::cout << "]" << std::endl;
 }
 
+// List
+void print(const SList<int>& list)
+{
+    SNode<int>* node = list.begin();
+    std::cout << "[";
+    for (int i = 0; i < list.size(); i++)
+    {
+        std::cout << node->data;
+        if (i != list.size() - 1)
+        {
+            std::cout << ", ";
+            node = node->next;
+        }
+    }
+    std::cout << "]" << std::endl;
+}
+
+// Main
 int main()
 {
-    // Array
-    Array<int> arr(1);
-    print(arr);
-    arr.push_back(1);
-    arr.push_back(2);
-    arr.push_back(3);
-    print(arr);
-    arr.pop_back();
-    print(arr);
-    arr.insert(1, 4);
-    print(arr);
-    arr[1] = 3;
-    print(arr);
-    arr.remove(1);
-    print(arr);
-    arr.clear();
-    print(arr);
+    // // Array
+    // Array<int> arr(1);
+    // print(arr);
+    // arr.push_back(1);
+    // arr.push_back(2);
+    // arr.push_back(3);
+    // print(arr);
+    // arr.pop_back();
+    // print(arr);
+    // arr.insert(1, 4);
+    // print(arr);
+    // arr[1] = 3;
+    // print(arr);
+    // arr.remove(1);
+    // print(arr);
+    // arr.clear();
+    // print(arr);
+
+    // Sngle-linked List
+    SList<int> list;
+    list.push_back(2);
+    print(list);
+    list.push_front(1);
+    print(list);
+    list.push_back(3);
+    list.pop_front();
+    list.pop_back();
+    print(list);
+    list.insert(1, 4);
+    print(list);
+    list.push_front(1);
+    list.push_front(0);
+    print(list);
+    SList<int> list2;
+    list2.push_back(5);
+    list2.push_back(6);
+    list.merge(list2, true);
+    print(list);
+    list.reverse();
+    print(list);
+    std::cout << list.endAt(2)->data << std::endl;
+    print(list);
+    // make a loop
+    SNode<int>* temporary = list.endAt(1);
+    temporary->next = list.begin()->next;
+    if (nullptr != list.hasLoop())
+        std::cout << list.hasLoop()->data << std::endl;
+    temporary->next = nullptr;
+    temporary = nullptr;
+
+    list.remove(1);
+    print(list);
+    list.clear();
+    print(list);
     return 0;
 }
